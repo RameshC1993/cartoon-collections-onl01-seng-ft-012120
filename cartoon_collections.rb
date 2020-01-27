a9dd8268dfe9ea5104ee4f2be2f14f053c5e3c3e
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-27 22:38:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,9 @@ def summon_captain_planet(array)
   return new_arr
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |names|
+    
 end
 
 def find_the_cheese# code an argument here
